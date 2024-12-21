29cab30d92e8cfcc47d36fed21663e717b693c63
RostSuslov <suslovros@gmail.com>
2024-12-21 11:11:51 +0200

    feat: show/hide password field

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('[type="tel"]').mask('+38(000)-00-00-000');
+    $('#password').hideShowPassword({
+        innerToggle: true,
+    });
 })
+
+ 
+const btnHistoryBack = document.querySelector('#history-back');
+const backHistory =()=>  window.history.back();
+btnHistoryBack.addEventListener('click', backHistory)
