387094755bce4d0aec77b72433466be899603a12
RostSuslov <suslovros@gmail.com>
2025-01-15 21:14:50 +0200

    feat: fixed header

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -5,7 +5,40 @@ $(document).ready(function() {
     });
 })
 
+
+// const btnHistoryBack = document.querySelector('#history-back');
+// const backHistory =()=>  window.history.back();
+// btnHistoryBack.addEventListener('click', backHistory)
+
+
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    if (window.scrollY > header.offsetTop) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+});
+
+
  
-const btnHistoryBack = document.querySelector('#history-back');
-const backHistory =()=>  window.history.back();
-btnHistoryBack.addEventListener('click', backHistory)
+
+
+
+
+
+
+// const time = 0
+// let greeting;
+
+// if (time < 4) {
+//     greeting = "Good night";
+// } else if (time < 12) {
+//     greeting = "Good morning";
+// } else if (time < 16) {
+//     greeting = "Good day";
+// } else if (time < 24) {
+//     greeting = "Good evening";
+// } 
+
+// console.log(greeting);
